8c3c9beb9409b950d5b2ea8899e21c4ca116a9ab
REDunix <105573621+colombiahack@users.noreply.github.com>
2024-05-19 09:12:14 -0500

    Update gata.sh

--- gata.sh
+++ gata.sh
@@ -260,7 +260,7 @@ menu() {
 }
 
 menu_ayuda() {
-    #printf "${v1}[${b}++${v1}]${v} GataBot-MD${b}: "
+    #printf "${v1}[${b}++${v1}]${v} UnixBot-MD${b}: "
 	read opcion
 	case $opcion in
             01|1)
